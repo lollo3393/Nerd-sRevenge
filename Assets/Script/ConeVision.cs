74238b4a4547fa97fa2d7427d571c4ab155f934f
Luca Picone <picone.luca777@gmail.com>
2025-05-08 14:05:13 +0200

    palle

--- Assets/Script/ConeVision.cs
+++ Assets/Script/ConeVision.cs
@@ -4,46 +4,96 @@ namespace Script
 {
     public class ConeVision : MonoBehaviour
     {
-        private LineRenderer lineRenderer;
+       
         [SerializeField] private Material material;
-        void OnTriggerStay(Collider other) {
-            
-            
+        [SerializeField] private Material allarmMaterial;
+        [SerializeField] private float pulseSpeed = 0.5f;
+        private bool playerFound = false;
+        private float timer;
+        public bool pulsingFlag= false;
+        private MeshRenderer mesh ;
+        private Material luce;
+        //private LineRenderer lineRenderer;
+        private AudioSource allertSound;
+        
+        void OnTriggerEnter(Collider other) {
             RaycastHit hit;
             Vector3 cameraPosition = transform.parent.position;
             if (Physics.Linecast(cameraPosition, other.transform.position, out hit))
             {
-                lineRenderer.SetPosition(0, cameraPosition);
-                lineRenderer.SetPosition(1, hit.point);
+                // lineRenderer.SetPosition(0, cameraPosition);
+                // lineRenderer.SetPosition(1, hit.point);
                 GameObject hitObject = hit.transform.gameObject;
                 if (hitObject.GetComponent<CharacterController>())
                 {
+                    allertSound.Play();
                     Debug.Log("ALLARME");
+                    mesh.material = allarmMaterial;
+                    playerFound = true;
                 }
                 else
                 {
-                    Debug.Log("Hello Word");
+                    Debug.Log("In range ma coperto ");
                 }
             }
         }
-        void OnTriggerExit(Collider other) {
-            
+        void OnTriggerStay(Collider other) {
+            if (!playerFound)
+            {
+                RaycastHit hit;
+                Vector3 cameraPosition = transform.parent.position;
+                if (Physics.Linecast(cameraPosition, other.transform.position, out hit))
+                {
+                    // lineRenderer.SetPosition(0, cameraPosition);
+                    // lineRenderer.SetPosition(1, hit.point);
+                    GameObject hitObject = hit.transform.gameObject;
+                    if (hitObject.GetComponent<CharacterController>())
+                    {
+                        allertSound.Play();
+                        Debug.Log("ALLARME");
+                        mesh.material = allarmMaterial;
+                        playerFound = true;
+                    }
+                    else
+                    {
+                        Debug.Log("In range ma coperto ");
+                    }
+                }
+            }
         }
         void Start()
         {
+            allertSound = GetComponent<AudioSource>();
+            mesh = gameObject.GetComponent<MeshRenderer>();
+            luce = mesh.material;
+            /*
             lineRenderer = gameObject.AddComponent<LineRenderer>();
             lineRenderer.positionCount = 2;
             lineRenderer.material = material;
             lineRenderer.startColor = Color.red;
             lineRenderer.endColor = Color.red;
             lineRenderer.startWidth = 1f;
-            lineRenderer.endWidth = 1f;
+            lineRenderer.endWidth = 1f;*/
         }
-
-        // Update is called once per frame
+        
         void Update()
         {
-        
-        }
-    }
-}
+            if (playerFound)
+            {
+                timer += Time.deltaTime;
+                if(timer > pulseSpeed){
+                    timer = 0;
+                    if (pulsingFlag) {
+                        mesh.material = allarmMaterial;
+                    }else{
+                        mesh.material = luce;
+                    }
+                    pulsingFlag = !pulsingFlag;
+                    timer += Time.deltaTime;
+                }
+                
+            }
+            
+        }//update
+    }//classe
+}//namespace
--- Assets/Script/cameraScript.cs
+++ Assets/Script/cameraScript.cs
@@ -9,60 +9,54 @@ public class cameraScript : MonoBehaviour
 	[SerializeField] public float maxRotation;
 	private float currentAngle = 0f;
 	private int direction = 1; // 1 = destra, -1 = sinistra
+	private bool m_Play;
+	AudioSource m_MyAudioSource;
+	bool flag = false;
 	
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-	    //StartCoroutine(CameraMovement());
+	    m_Play = true;
+	    m_MyAudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
 	    {
+		    if (m_Play == true && flag == false)
+		    {
+			    //Play the audio you attach to the AudioSource component
+			    m_MyAudioSource.Play();
+			    //Ensure audio doesn’t play more than once
+			    flag = true;
+		    }
+		   
 		    float rotationStep = speed * Time.deltaTime * direction*10;
 		    currentAngle += rotationStep;
 
 		    // Inversione direzione se raggiunge i limiti
 		    if (currentAngle >= maxRotation)
 		    {
+			    m_Play = false;
 			    currentAngle = maxRotation;
 			    direction = -1;
 		    }
 		    else if (currentAngle <= -maxRotation)
 		    {
+			    m_Play = false;
 			    currentAngle = -maxRotation;
 			    direction = 1;
 		    }
+		    
+		    if(m_Play == false)
+		    {
+			    m_MyAudioSource.Stop();
+			    flag = false;
+		    }
 
 		    transform.localRotation = Quaternion.Euler(transform.localEulerAngles.x, currentAngle, 0f);
+		    m_Play = true;
 	    }
-    }
-	
-	IEnumerator CameraMovement(){
-		
-		
-		float elapsedTime = 0;
-		
-		float currentY = transform.eulerAngles.y ;
-		while (gameObject.activeSelf)
-		{
-			elapsedTime = Time.deltaTime;
-			if (Mathf.Abs(currentY) >= Mathf.Abs(maxRotation))
-			{
-				maxRotation *= -1;
-				elapsedTime += Time.deltaTime;
-			}
-			
-			currentY = Mathf.Lerp(-maxRotation,maxRotation , elapsedTime / speed);
-			elapsedTime += Time.deltaTime;
-			transform.localEulerAngles = new Vector3(0,currentY,0);
-			
-			yield return new WaitForEndOfFrame();
-		}
+    }//update
+}//class	
 
-		
-	}	
-	
-}
