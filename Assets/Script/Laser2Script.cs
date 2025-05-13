37932847ae6e4f279b4e1f434f2c3c4c865f2795
Luca Picone <picone.luca777@gmail.com>
2025-05-13 15:33:11 +0200

    laserSlider

--- Assets/Script/Laser2Script.cs
+++ Assets/Script/Laser2Script.cs
@@ -6,16 +6,15 @@ using UnityEngine;
 
 public class laser2Script : MonoBehaviour
 {
+    
+    // posizionare l'oggetto a meta del percorso che dovra' compiere visto che si muove con un comportamento sinusoidale
     [SerializeField]
     float maxAnimationDuration = 1.0f;
-    [SerializeField]
-    float minHeight ;
+    [SerializeField] float minHeight ;
     [SerializeField] private float maxHeight ; 
-    [SerializeField] private  GameObject laser;
-    [SerializeField] private  LineRenderer line;
     [SerializeField] private alarmController alarmController;
+    [SerializeField] float oscillazione;
     private Vector3 startpos;
-    private Vector3 endpos;
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CharacterController>())
@@ -23,46 +22,19 @@ public class laser2Script : MonoBehaviour
             alarmController.SendMessage("StartAlarm");
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
     void Update()
     {
-       // float currentY = ((line.GetPosition(0).y + 0.45f ) * maxHeight)/0.9f;
-        // laser.transform.position = new Vector3(laser.transform.position.x, currentY, laser.transform.position.z);
-        float newY = startpos.y + Mathf.Sin(Time.time * maxAnimationDuration) * maxHeight;
+      
+        float newY = startpos.y + Mathf.Sin(Time.time * maxAnimationDuration) * oscillazione;
         transform.position = new Vector3(startpos.x, newY, startpos.z);
     }
 
     void Start()
     {
-        startpos = laser.transform.position;
-        minHeight = startpos.y;
-        endpos =  new Vector3(startpos.x , maxHeight, startpos.z);
-          
-       // StartCoroutine(LaserAnimation());
+        startpos = transform.position;
+        oscillazione = (Mathf.Abs(minHeight)+maxHeight)/2;
+        
     }
-    
-    
-
-
-
-    IEnumerator LaserAnimation()
-    {
-        float currentY = startpos.y;
-        float elapsedTime = 0;
-        while (gameObject.activeSelf)
-        {
-            if (Mathf.Abs(currentY) >= Mathf.Abs(maxHeight))
-            {
-                (minHeight, maxHeight) = (maxHeight, minHeight);
-                (startpos,endpos) = (endpos, startpos);
-                //elapsedTime = 0;
-                elapsedTime += Time.deltaTime;
-            }
-            transform.position = Vector3.Lerp(startpos, endpos, (maxAnimationDuration/ elapsedTime));
-            elapsedTime += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-    }
-
     
 }
