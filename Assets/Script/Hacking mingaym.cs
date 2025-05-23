f5f1972f5ecf695498013fe883ada724d0522cbd
Luca Picone <picone.luca777@gmail.com>
2025-05-23 15:20:32 +0200

    thfh

--- Assets/Script/Hacking mingaym.cs
+++ Assets/Script/Hacking mingaym.cs
@@ -1,14 +1,38 @@
+using TMPro;
 using UnityEngine;
 
+public enum difficolta
+{
+    facile,
+    medio, 
+    difficile
+}
 public class Hackingmingaym : MonoBehaviour
 {
     private string funzione;
     private int nVar;
+    [SerializeField] private TextMeshProUGUI tmp;
+    [SerializeField] private difficolta diff;
+    [SerializeField] private GameObject gridLayout;
+    [SerializeField] private GameObject switchPrefab;
     
     private void Start()
     {
-        nVar = 3;
+        switch (diff)
+        {
+            case difficolta.facile:
+                nVar = 2;
+                break;
+            case difficolta.medio:
+                nVar = 3;
+                break;
+            case difficolta.difficile:
+                nVar = 4;
+                break;
+            
+        }
         funzione = generaFunzione();
+        tmp.text = "f = "+funzione;
         Debug.Log(funzione);
 
     }
